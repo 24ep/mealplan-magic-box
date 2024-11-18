6f8eb64001444c110be85eda76311d794be6f0f1
Jaroonwit Poolnai <MD3770@qsncc.com>
2024-11-18 20:21:59 +0700

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "http://10.0.10.55/",
+    host: "::",
     port: 80,
   },
   plugins: [
